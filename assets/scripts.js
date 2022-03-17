52c5514a5d7a39b80433c974df9ecbac4a61e5c9
alvesxdani <dani.alvesm22@gmail.com>
2022-03-17 18:03:33 -0300

    Add verifica() function

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,33 @@
 var resultado = document.getElementById("resultado");
 
+resultado.style.display = "none";
+
+// FUNÇÃO DE VERIFICAR O PALÍNDROMO
+function verificar() {
+    let phrase = document.getElementById("palindromo").value;
+
+    let otherPhrase = phrase.split("").reverse().join("");
+
+    if(phrase === "") {
+        resultado.innerHTML = "⚠ Escreva algo!";
+    } else if (phrase === otherPhrase) {
+        resultado.innerHTML = "✅ É um palíndromo!";
+    } else if (phrase != otherPhrase) {
+        resultado.innerHTML = "❌ Não é um palíndromo.";
+    }
+
+    console.log(phrase);
+    
+    console.log(otherPhrase);
+
+    resultado.style.display = "block";
+}
+
+// FUNÇÃO PARA LIMPAR O INPUT
 function limpar() {
-    document.getElementById("palindromo").value = "";
+    let phrase = document.getElementById("palindromo").value = "";
+
     resultado.innerHTML = "";
+
+    resultado.style.display = "none";
 }
